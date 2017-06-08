36fc3675311a62f55976d45f1c46ccc40879e043
tahbristol <tahbristol@gmail.com>
2017-06-08 12:59:58 -0400

    fished login, added new request form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,27 @@ class UsersController < ApplicationController
   end
 
   get '/signup'do
+  if logged_in?(session)
+    @user = User.find_by_id(session[:user_id])
+    redirect to '/users/#{@user.slug}'
+  else
     erb :'/users/signup'
+  end
   end
 
   get '/users/:slug'do
+  if logged_in?(session)
     @user = User.find_by_slug(params[:slug])
     erb :'/users/show'
+  else
+    redirect to '/signup'
   end
+  end
+
+  get '/login'do
+   erb :'/users/login'
+  end
+
 
 
 
@@ -24,6 +38,7 @@ class UsersController < ApplicationController
 post '/signup'do
   if params[:password] == params[:password_confirm]
     @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
     redirect to "/users/#{@user.slug}"
   else
     flash[:message] = "Passwords do not match."
@@ -31,6 +46,25 @@ post '/signup'do
 
   end
 end
+
+
+post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/users/#{@user.slug}"
+    else
+      flash[:message] = "Those credentails are not found in our system."
+      redirect to '/login'
+    end
+end
+
+
+post '/logout'do
+    session.clear
+    redirect to '/login'
+  end
+
 
 
 

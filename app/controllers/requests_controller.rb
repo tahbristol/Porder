4bcd2c39d972e075f58eb0ce8b434933bbb6c168
tahbristol <tahbristol@gmail.com>
2017-06-11 12:09:25 -0400

    changed request output to table, css table, changed requests/new route, and a few inputs to buttons with btn-primary class

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,14 +4,14 @@ class RequestsController < ApplicationController
 
   get '/requests'do
     if logged_in?(session)
-      @user = User.find_by_id(session[:user_id]) 
+      @user = User.find_by_id(session[:user_id])
       erb :'requests/index'
     else
       redirect to'/signup'
     end
   end
 
-  get '/new-request'do
+  get '/requests/new'do
     if logged_in?(session)
       erb :'/requests/new'
     else
@@ -27,7 +27,7 @@ class RequestsController < ApplicationController
 
   ################################################
 
-  post '/new-request'do
+  post '/requests/new'do
 
     @user = User.find_by_id(session[:user_id])
     @user.requests.build(item: params[:item], vendor: params[:vendor], quantity: params[:quantity], price: params[:price], url: params[:url])

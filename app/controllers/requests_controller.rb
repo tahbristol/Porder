e246974b05b5a18019a1c7f3c839c06ed89fbc44
tahbristol <tahbristol@gmail.com>
2017-06-22 23:27:27 -0400

    redirect if no requests are checked for deletion

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -46,16 +46,33 @@ end
   post '/requests/delete'do
       @user = User.find_by_id(session[:user_id])
 
+        if !params[:request]
+          redirect to "/requests/#{@user.slug}/show"
+        else
     params[:request][:request_ids].each do |request|
       request = Request.find_by_id(request)
       request.destroy
     end
+  end
     redirect to "/requests/#{@user.slug}/show"
    end
 
    post '/requests/edit'do
+    @user = User.find_by_id(session[:user_id])
 
 
-    end 
+    params[:request].each do |request|
+
+      edit_request = Request.find_by_id(request[:id])
+    edit_request.item = request[:item] unless request[:item].empty?
+    edit_request.vendor =  request[:vendor] unless request[:vendor].empty?
+    edit_request.quantity =  request[:quantity] unless request[:quantity].empty?
+    edit_request.price =  request[:price] unless request[:price].empty?
+    edit_request.url = request[:url] unless request[:url].empty?
+
+    edit_request.save
+end
+      redirect to "/requests/#{@user.slug}/show"
+    end
 
 end

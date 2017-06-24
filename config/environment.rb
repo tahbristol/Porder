896ac46af0e1d7a5bf7def2d6be2ad688d0a0431
tahbristol <tahbristol@gmail.com>
2017-06-24 15:59:50 -0400

    remove all from env except db postres

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,16 @@
+=begin
 ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-=begin
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 =end
+require 'bundler/setup'
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 require_all 'app'

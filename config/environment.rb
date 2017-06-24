30acad945300ecb0e794a51c4140f2863bbd2a9f
tahbristol <tahbristol@gmail.com>
2017-06-24 02:17:27 -0400

    code for postgres db for heroku deployment

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,6 @@ require 'bundler/setup'
 
 Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.logger = Logger.new(STDOUT)
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 require_all 'app'

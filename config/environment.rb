94e404d52e2d6ff70e991035b22a2e88cd0b0520
tahbristol <tahbristol@gmail.com>
2017-06-24 23:46:54 -0400

    changed database setup for production and development

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,18 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
-=begin
+
+configure :development do
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+end
 
-=end
+
+configure :production do
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
+
+end
 require_all 'app'

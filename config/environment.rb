2e06da5a8b0486a4978697f205a597ccff68acdb
Tyler B <tahbristol@users.noreply.github.com>
2018-04-28 13:42:53 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,17 +7,13 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 configure :development do
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
 end
 
-
 configure :production do
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
 require_all 'app'
